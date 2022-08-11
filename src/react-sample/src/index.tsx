8d4651897553aa6a4d97f1cdcb275857c0c07c2d
dekihi- <none>
2022-08-11 17:43:15 +0900

    learned useMemo

--- src/react-sample/src/index.tsx
+++ src/react-sample/src/index.tsx
@@ -1,46 +1,38 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
 import React from "react";
-import ReactDOM from "react-dom";
+// ReactDOM.renderは、React18非対応 (https://zenn.dev/kohski/articles/create-react-app-error-v18, https://qiita.com/chatrate/items/4ef1058ddc0566e12cbc)
+import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Hello from "./components/Hello";
-import { Parent as ContainerSampleParent } from "./components/ContainerSample";
-import { Page } from "./components/ContextSample";
-import { Counter as UseStateCounter } from "./components/UseStateSample";
-import { Counter as UseReducerCounter } from "./components/UseReducerSample";
-import { Parent } from "./components/Parent";
-import { Parent as UseCallbackParent } from "./components/UseCallbackSample";
+import reportWebVitals from "./reportWebVitals";
+// import App from "./App";
+// import Hello from "./components/Hello";
+// import { Parent as ContainerSampleParent } from "./components/ContainerSample";
+// import { Page } from "./components/ContextSample";
+// import { Counter as UseStateCounter } from "./components/UseStateSample";
+// import { Counter as UseReducerCounter } from "./components/UseReducerSample";
+// import { Parent } from "./components/Parent";
+// import { Parent as UseCallbackParent } from "./components/UseCallbackSample";
+import { UseMemoSample } from "./components/UseMemoSample";
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <Hello />
-  //   <ContainerSampleParent />
-  //   <Page />
-  //   <Page />
-  //   <UseStateCounter initialValue={0} />
-  //   <UseReducerCounter initialValue={0} />
-  //   <Parent />
-    <UseCallbackParent />,
-  //   <App />
-  // </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    //   <Hello />
+    //   <ContainerSampleParent />
+    //   <Page />
+    //   <Page />
+    //   <UseStateCounter initialValue={0} />
+    //   <UseReducerCounter initialValue={0} />
+    //   <Parent />
+    //   <UseCallbackParent />,
+    <React.StrictMode>
+      <UseMemoSample />
+    </React.StrictMode>
+    //   <App />
+  );  
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
