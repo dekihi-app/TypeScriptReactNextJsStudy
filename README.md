a1f4821bcda55c2039b3094be64da7a234ee0413
dekihi- <none>
2022-08-28 03:58:44 +0900

    new project

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 『TypeScript と React/Next.js でつくる 実践 Web アプリケーション開発』
 
 - [正誤表とソースコード](https://gihyo.jp/book/2022/978-4-297-12916-3/support)
+- [有識者の書評](https://blog.uhy.ooo/entry/2022-08-11/typescript-react-nextjs/)
---
+++ src/nextjs-gihyo-book/src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './colors';
+import fontSizes from './fontSizes';
+import letterSpacings from './letterSpacings';
+import lineHeights from './lineHeight';
+import space from './spaces';
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const;
